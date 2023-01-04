fe273e057a7823381ec8ae5d8cf565c79eeb0cd5
akshay0828 <Akshay.K@valtech.com>
2023-01-04 18:32:15 +0530

    Apache Tomcat-Deployed with IP-address-192.168.141.36

--- Spice-Land/src/main/java/com/valtech/spring/security/SpringSecurityTestApplication.java
+++ Spice-Land/src/main/java/com/valtech/spring/security/SpringSecurityTestApplication.java
@@ -2,10 +2,20 @@ package com.valtech.spring.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringSecurityTestApplication {
+public class SpringSecurityTestApplication extends SpringBootServletInitializer  {
 
+	
+	@Override  
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
+	{ 
+		
+		
+	return application.sources(SpringSecurityTestApplication.class);  
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityTestApplication.class, args);
 	}
--- Spice-Land/src/main/webapp/WEB-INF/views/register.jsp
+++ Spice-Land/src/main/webapp/WEB-INF/views/register.jsp
@@ -17,7 +17,7 @@
 	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
 	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
 	crossorigin="anonymous">
-	  <link rel="stylesheet" type="text/css" href="/css/register.css" />
+	  <link rel="stylesheet" type="text/css" href="css/register.css" />
 </head>
 
 <body class="body">
